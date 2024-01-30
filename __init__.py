32f1a521d1598bee653d68d499ffe3a7f66a52bf
datleXx <dat91fitzz@gmail.com>
2024-01-30 14:35:03 +1100

    Create Image pipeline and basic video pipeline

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .dat import image_pipeline
+from .dat import video_pipeline
+from .dat import video_streaming_pipeline
---
+++ dat/test.py
@@ -0,0 +1,22 @@
+from aiko_services import ImageCV
+import cv2 
+import time
+
+cap = cv2.VideoCapture(0)
+test = cap 
+if not cap.isOpened(): 
+    print("Error")
+    exit()
+
+while True : 
+    ret, frame = cap.read()
+    if not ret: 
+        print("End of stream ? Exitting ... ")
+        break
+    cv2.imshow("Obj_detect",frame)
+    # time.sleep(0.1)
+    if cv2.waitKey(1) == ord("q"): 
+        break
+print(test.isOpened())
+cap.release()
+cv2.destroyAllWindows()
