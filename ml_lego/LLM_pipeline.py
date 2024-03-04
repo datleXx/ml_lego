7f59bc48a05f7a36d5bfb14e7cdba668be94f50d
datleXx <129921351+datleXx@users.noreply.github.com>
2024-03-05 01:38:16 +1100

    Revert "Update LLM pipeline"

--- ml_lego/LLM_pipeline.py
+++ ml_lego/LLM_pipeline.py
@@ -14,41 +14,22 @@ from typing import Tuple
 
 from aiko_services import aiko, PipelineElement
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-
-import torch 
+from transformers import pipeline
 
 _LOGGER = aiko.logger(__name__)
-
-model_id = "meta-llama/Llama-2-7b-hf"
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
+generator = pipeline(model="gpt2")
 
 # --------------------------------------------------------------------------- #
 
-class PE_LLM(PipelineElement):
+class PE_GPT2(PipelineElement):
     def __init__(self, context):
-        context.set_protocol("LLM")
+        context.set_protocol("Llama2-7B-hf")
         context.get_implementation("PipelineElement").__init__(self, context)
 
     def process_frame(self, context, text) -> Tuple[bool, dict]:
-        _LOGGER.info(f"{model_id}: {context}, in: {text}")
-        model_inputs = tokenizer([f"{text}"], return_tensors="pt").to("cuda")
-
-        # Setting `max_new_tokens` allows you to control the maximum length
-        generated_ids = model.generate(**model_inputs, max_new_tokens=50)
-        output = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-        _LOGGER.info(f"{model_id}: {context}, out: {output}")
-        return True, {"output": output}
+        _LOGGER.info(f"PE_GPT2: {context}, in: {text}")
+        output = generator(text, do_sample=False)
+        _LOGGER.info(f"PE_GPT2: {context}, out: {output[0]['generated_text']}")
+        return True, {"output": output[0]['generated_text']}
 
 # --------------------------------------------------------------------------- #
